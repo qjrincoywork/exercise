1f887e61083ca5cc4ddd9976fbbef60062e77bb6
qjrincoywork <quirjohnincoy.work@gmail.com>
2020-03-19 12:02:00 +0800

    removed previous orm, added the mvc

--- orm/index.php
+++ orm/index.php
@@ -1,28 +1,23 @@
 <?php
-$root = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
-$script_name = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-// define ('__SALT__', 'Fho8G**g&0ds43syK0PKPph&^D64fi7g1k-90`j*G&7IVGD');
-define ('__ROOT__', $root.$script_name);
-define ('__SITE_NAME__', 'YNS Exercise');
-define ('__ROOT_PATH__', realpath($_SERVER['DOCUMENT_ROOT'].'/'.$script_name));
-define ('__HOSTNAME__', 'localhost');
-define ('__USERNAME__', 'root');
-define ('__PASSWORD__', 'vagrant');
-define ('__DATABASENAME__', 'yns');
-spl_autoload_register(function ($className) {
-    
-    if (file_exists('System/' . $className . '.php')) { 
-        require_once 'System/' . $className . '.php'; 
-    }
-	else if (file_exists('Controllers/' . $className . '.php')) { 
-        require_once 'Controllers/' . $className . '.php'; 
-    }
-	else if (file_exists('Models/' . $className . '.php')) { 
-        require_once 'Models/' . $className . '.php'; 
-    }
-    else if (file_exists($className . '.php')) {
-        require_once $className . '.php'; 
-    }
-});
+define("URL", "http://$_SERVER[HTTP_HOST]");
 
-new Bootstrap();
+define('DATABASE_HOST','localhost');
+define('DATABASE_USER','root');
+define('DATABASE_PASS','');
+define('DATABASE_NAME','yns');
+
+include_once "lib/session.php";
+include_once "lib/database.php";
+include_once "lib/view.php";
+include_once "lib/route.php";
+include_once "lib/controller.php";
+include_once "lib/model.php";
+
+class Index 
+{
+    public function __construct() {
+        new Route();
+    }
+}
+
+new Index();
--- orm/views/components/header1.php
+++
@@ -1,4 +0,0 @@
-<?php
-// defined('__ROOT__') OR exit('No direct script access allowed');
-
-echo "header.php";
